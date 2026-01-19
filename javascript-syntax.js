ff535bab6bf1d10177383814fcf79aa609bc62fd
louwdemand <louwdemand@gmail.com>
2026-01-19 16:17:15 +0200

    JavaScript Syntax

---
+++ javascript-syntax.js
@@ -0,0 +1,98 @@
+Syntax Rules
+Syntax are the rules how programs must be constructed:
+// How to Declare variables:
+let x = 5;
+let y = 6;
+
+// How to Compute values:
+let z = x + y;
+
+// I am a Comment. I do Nothing
+JavaScript Values
+The JavaScript syntax defines two types of values:
+
+Literals (Fixed values)
+Variables (Variable values)
+JavaScript Literals
+The most important syntax rules for literals (fixed values) are:
+
+Numbers are written with or without decimals:
+
+Example
+10.50
+
+1001
+Strings are text, written within double or single quotes:
+
+Example
+"John Doe"
+
+'John Doe'
+JavaScript Keywords
+JavaScript keywords are used to defines actions to be performed.
+
+The let and const keywords create variables:
+
+Example
+let x = 5;
+
+const fname = "John";
+Note
+JavaScript keywords are case-sensitive.
+
+JavaScript does not interpret LET or Let as the keyword let.
+
+JavaScript Variables
+Variables are containers for storing data values.
+
+Variables must be identified with unique names.
+
+Example
+// Define x as a variable
+let x;
+
+// Assign the value 6 to x
+x = 6;
+JavaScript Identifiers
+An identifier is the name you give to a variable.
+
+Rules for identifiers:
+
+Must start with a letter, _, or $
+Can contain digits after the first character
+Cannot be a reserved keyword (let, const, if, etc.)
+Are case-sensitive
+JavaScript Operators
+JavaScript assignment operators (=) assign values to variables:
+
+Example
+let x = 5;
+let y = 6;
+let sum = x + y;
+JavaScript uses arithmetic operators ( + - * / ) to compute values:
+
+Example
+5 * 10
+JavaScript Expressions
+An expression is a combination of values, variables, and operators, which computes to a value.
+
+Examples
+(5 + 6) * 10 evaluates to 110:
+
+(5 + 6) * 10
+Expressions can also contain variable:
+
+x * 10
+"John" + " " + "Doe", evaluates to "John Doe":
+
+"John" + " " + "Doe"
+REMOVE ADS
+
+JavaScript is Case Sensitive
+JavaScript identifiers are case sensitive.
+
+The variables lastName and lastname, are different variables:
+
+Example
+let lastName = "Doe";
+let lastname = "Peterson";
