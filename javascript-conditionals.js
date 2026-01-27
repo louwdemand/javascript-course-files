96e1984644fad87cb1a79b32032d800da00d0b76
louwdemand <louwdemand@gmail.com>
2026-01-27 12:50:48 +0200

    JavaScript Conditionals

---
+++ javascript-conditionals.js
@@ -0,0 +1,67 @@
+JavaScript Conditionals
+Conditional Statements
+Conditional Statements allow us to perform different actions for different conditions.
+
+Conditional statements run different code depending on true or false conditions.
+
+Conditional statements include:
+
+if
+if...else
+if...else if...else
+switch
+ternary (? :)
+When to use Conditionals
+Use if to specify a code block to be executed, if a specified condition is true
+Use else to specify a code block to be executed, if the same condition is false
+Use else if to specify a new condition to test, if the first condition is false
+Use switch to specify many alternative code blocks to be executed
+Use (? :) (ternary) as a shorthand for if...else
+The if Statement
+Use if to specify a code block to be executed, if a specified condition is true.
+
+Syntax
+if (condition) {
+  // code to execute if the condition is true
+}
+The else Statement
+Use else to specify a code block to be executed, if the same condition is false.
+
+Syntax
+if (condition) {
+  // code to execute if the condition is true
+} else {
+  // code to execute if the condition is false
+}
+The else if Statement
+Use else if to specify a new condition to test, if the first condition is false.
+
+Syntax
+if (condition1) {
+  // code to execute if condition1 is true
+} else if (condition2) {
+  // code to execute if the condition1 is false and condition2 is true
+} else {
+  // code to execute if the condition1 is false and condition2 is false
+}
+REMOVE ADS
+
+The switch Statement
+Use switch to specify many alternative code blocks to be executed.
+
+Syntax
+switch(expression) {
+  case x:
+    // code block
+    break;
+  case y:
+    // code block
+    break;
+  default:
+    // code block
+}
+Ternary Operator (? :)
+Use (? :) (ternary) as a shorthand for if...else.
+
+Example
+condition ? expression1 : expression2
